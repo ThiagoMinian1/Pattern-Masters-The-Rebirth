a4f9e51fb05391900905aaa2969f64cefffd3bb2
LeanCorti <leancorti2006@gmail.com>
2026-06-10 09:16:42 -0300

    templateMethod

---
+++ SistemaHotelero/src/templateMethod/PagoTransferencia.java
@@ -0,0 +1,32 @@
+package templateMethod;
+import com.HotelReservas.Modelos.Reserva;
+
+public class PagoTransferencia extends ProcesadorPago {
+
+    private String cbu;
+    private String alias;
+
+    public PagoTransferencia(String cbu, String alias) {
+        this.cbu = cbu;
+        this.alias = alias;
+    }
+
+    @Override
+    protected void validarDatos(Reserva reserva) {
+        if (cbu == null || cbu.length() != 22)
+            throw new IllegalArgumentException("CBU inválido.");
+        System.out.println("Transferencia validada. CBU: " + cbu + " | Alias: " + alias);
+    }
+
+    @Override
+    protected void realizarPago(double monto) {
+        System.out.println("Transferencia bancaria enviada por $" + String.format("%.2f", monto)
+                + " al alias: " + alias);
+    }
+
+    @Override
+    protected String getMetodoPago() {
+        return "Transferencia Bancaria";
+    }
+}
+
