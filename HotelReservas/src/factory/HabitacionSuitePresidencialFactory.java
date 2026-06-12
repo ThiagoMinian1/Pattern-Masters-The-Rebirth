2c95b812803394f52d9ccd37c8f7e72edf5e3b12
AgosLopez <solagos306@gmail.com>
2026-06-12 13:54:27 -0300

    patron factory

--- HotelReservas/src/factory/HabitacionSuitePresidencialFactory.java
+++ HotelReservas/src/factory/HabitacionSuitePresidencialFactory.java
@@ -2,10 +2,10 @@ package com.HotelReservas.factory;
  
 import com.HotelReservas.Modelos.Habitacion;
  
-public class HabitacionDobleFactory extends HabitacionFactory {
+public class HabitacionSuitePresidencialFactory extends HabitacionFactory {
  
     @Override
     public Habitacion crearHabitacion(int numero) {
-        return new Habitacion(numero, "DOBLE", 2, 90.0);
+        return new Habitacion(numero, "SUITE PRESIDENCIAL", 4, 500.0);
     }
 }
