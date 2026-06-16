a3db881bde071ecc3381d83c8962a4bbac5d155c
ThiagoMinian <tminian@uade.edu.ar>
2026-06-16 12:28:51 -0300

    interfaz

--- HotelReservas/src/decorator/EstadiaDecorable.java
+++
@@ -1,7 +0,0 @@
-package com.HotelReservas.decorator;
- 
-// Componente base que toda estadía decorable debe implementar
-public interface EstadiaDecorable {
-    double calcularCosto();
-    String getDescripcion();
-}
